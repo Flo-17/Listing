f59160bf6809f151a6aa97065da563218519b85a
Flo-17 <cammarata.florian@hotmail.com>
2021-08-30 17:19:52 +0200

    Premier commit du projet

---
+++ server.js
@@ -0,0 +1,54 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let session = require('express-session');
+const Message = require('./models/message');
+
+require('dotenv').config({path: './config/.env'})
+let app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+})
+
+// Moteur de template
+app.set('view engine' , 'ejs')
+
+// Middleware
+app.use('/assets', express.static('public'))
+app.use(bodyParser.urlencoded({ urlencoded : false }))
+app.use(bodyParser.json())
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+  }))
+app.use(require('./middlewares/flash'))
+
+// Routes
+app.get('/', (request,response) =>{
+    
+    let Message = require('./models/message')
+    Message.all(function(messages) {
+
+        response.render('pages/index', {messages: messages})
+
+    })
+})
+
+app.post('/', (request,response) =>{
+
+    if(request.body.message === undefined || request.body.message === '')
+    {
+        request.flash('error', "Vous n'avez pas entré de message.")    
+        response.redirect('/')    
+    }
+    else
+    {
+        let Message = require('./models/message')
+        Message.create(request.body.message, function() {
+            request.flash('success', "Merci !")
+            response.redirect('/')
+        })
+    }
+})
