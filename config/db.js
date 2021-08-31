7f451060bbcc499d0473d20806016f776b827833
Flo-17 <cammarata.florian@hotmail.com>
2021-08-31 13:47:33 +0200

    Update pour Heroku

--- config/db.js
+++ config/db.js
@@ -1,9 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'test123*',
-  database : 'TutoNodeJS'
+  //host     : 'localhost',
+  user     : 'b7fd61537111ca',
+  password : '74cd2cd7',
+  database : 'TutoNodeJS',
+  "insecureAuth" : true
 });
  
 connection.connect(function(err) {
@@ -14,14 +15,5 @@ connection.connect(function(err) {
  
   console.log('connected as id ' + connection.threadId);
 });
-
-var del = connection._protocol._delegateError;
-connection._protocol._delegateError = function(err, sequence){
-  if (err.fatal) {
-    console.trace('fatal error: ' + err.message);
-  }
-  return del.call(this, err, sequence);
-};
-
 
 module.exports = connection 
