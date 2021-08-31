5d6707bc8db73de63a5bbb4d084f76b5a20a9406
Flo-17 <cammarata.florian@hotmail.com>
2021-08-31 15:40:57 +0200

    Modif fichier DB avec des pool pour test Heroku

--- config/db.js
+++ config/db.js
@@ -1,36 +1,19 @@
 var mysql      = require('mysql');
-var db_config = {
+var pool  = mysql.createPool({
+  connectionLimit : 1,
   host     : 'eu-cdbr-west-01.cleardb.com',
   user     : 'b7fd61537111ca',
   password : '74cd2cd7',
   database : 'heroku_e8e29ea723b7f04',
   "insecureAuth" : true
-};
+});
 
-var connection = mysql.createConnection(db_config)
+pool.getConnection(function(err, connection) {
+  if (err) throw err; // not connected!
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
     console.log('connected as id ' + connection.threadId);
-                                          // process asynchronous requests in the meantime.
-    })                                    // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
+    // When done with the connection, release it.
+    connection.release();
+});
 
-handleDisconnect();
-
-module.exports = connection 
+module.exports = pool 
